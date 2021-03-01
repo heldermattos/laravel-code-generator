e78ec5acef954f786a721e1ca1e99ee8c9e0788d
Helder C. Mattos <sysconexao@gmail.com>
2021-02-28 21:30:50 -0300

    refactor: CodegenApiCommand defined

---
+++ src/Console/CodegenApiCommand.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace VictorYoalli\LaravelCodeGenerator\Console;
+
+use Illuminate\Console\Command;
+use VictorYoalli\LaravelCodeGenerator\CodeGenerator;
+use VictorYoalli\LaravelCodeGenerator\ModelLoader;
+
+class CodegenApiCommand extends Command
+{
+    protected $signature = 'codegen:api {model} ' . 
+        '{--namespace=App\Models : Models Namespace} ' .
+        '{--t|template= : template location} ' .
+        '{--o|outfile= : Output file location} ' .
+        '{--theme=api : Theme} ' .
+        '{--version=V1 : Version}';
+
+    protected $description = 'A Laravel Code Generator based on your Models.';
+
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    public function handle(ModelLoader $loader, CodeGenerator $generator)
+    {
+        // many changes to make, massa
+        $model = $this->argument('model');
+
+        if (empty($model)) {
+            return;
+        }
+
+        $theme = $this->option('theme');
+        $version = $this->option('version');
+
+        $options = compact(['theme', 'version']);
+        
+        $namespace = rtrim($this->option('namespace'), '\\');
+        $model = "{$namespace}\\{$model}";
+        $m = $loader->load($model);
+
+        $this->generate($m, $options, $theme, true);
+    }
+
+    public function generate($m, $options, $theme, $force)
+    {
+        $option = (object) $options;
+        $folder = CodeHelper::plural($m->name);
+        $base_path = "app/Api/{$option->version}/{$folder}";
+
+        printif('API Controller', CodeGenerator::generate(
+            $m,
+            $theme . '/Http/Controllers/ModelApiController',
+            "{$base_path}/Http/Controllers/{$folder}Controller.php", $force, $options
+        ));
+
+        printif('API Resource', CodeGenerator::generate(
+            $m,
+            $theme . '/Http/Resources/Model',
+            "{$base_path}/Http/Resources/{$m->name}.php", $force, $options
+        ));
+
+        printif('API Collection', CodeGenerator::generate(
+            $m,
+            $theme . '/Http/Resources/ModelCollection',
+            "{$base_path}/Http/Resources/{$m->name}Collection.php", $force, $options
+        ));
+
+        printif('API Routes', CodeGenerator::generate(
+            $m,
+            $theme . '/Http/Routes/index',
+            "{$base_path}/Http/Routes/index.php", $force, $options
+        ));
+
+        printif('API Repository', CodeGenerator::generate(
+            $m,
+            $theme . '/Repository/ModelRepository',
+            "{$base_path}/Repository/{$folder}Repository.php", $force, $options
+        ));
+    }
+}
