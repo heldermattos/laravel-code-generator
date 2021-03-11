a6beca05cac9fda5f07a195de0fae7dfda2c95db
Helder C. Mattos <sysconexao@gmail.com>
2021-03-11 15:20:24 -0300

    refactor: template updated

--- resources/views/apiconfig/Http/Controllers/ModelApiController.blade.php
+++ resources/views/apiconfig/Http/Controllers/ModelApiController.blade.php
@@ -2,13 +2,76 @@
 
 namespace App\Api\{{$options->version}}\{{CodeHelper::plural($model->name)}}\Http\Controllers;
 
-use App\Api\{{$options->version}}\{{CodeHelper::plural($model->name)}}\Repository\{{CodeHelper::plural($model->name)}}Repository;
-use App\Http\Controllers\BaseApiController;
+use App\Traits\HttpService;
+use Illuminate\Http\Request;
+use Laravel\Lumen\Routing\Controller;
 
-class {{$model->name}}Controller extends BaseApiController
+class {{$model->name}}Controller extends Controller
 {
-    public function __construct({{CodeHelper::plural($model->name)}}Repository $model)
+    use HttpService;
+
+    protected $msBaseUrl = "";
+    
+    protected $msSecretKey = "";
+
+    protected $request = "";
+
+    public function __construct(Request $request)
     {
-        $this->model = $model;
+        $this->msBaseUrl = config('microservices.{{$model->name}}.base_url');
+
+        $this->msSecretKey = config('microservices.{{$model->name}}.secret_key');
+
+        $this->request = $request;
+    }
+    
+    public function list()
+    {
+        return $this->get();
+    }
+
+    public function item()
+    {
+        return $this->get();
+    }
+
+    public function create()
+    {
+        return $this->post();
+    }
+
+    public function update()
+    {
+        return $this->put();
+    }
+
+    public function patch()
+    {
+        return $this->patch();
+    }
+
+    public function delete()
+    {
+        return $this->delete();
+    }
+
+    public function listRelative(Request $request, $id, $rel)
+    {
+
+    }
+
+    public function createRelative(Request $request, $id, $rel)
+    {
+
+    }
+
+    public function updateRelative(Request $request, $id, $rel)
+    {
+
+    }
+
+    public function uploadFiles(Request $request, $id)
+    {
+
     }
 }
